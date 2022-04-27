c297ee5943d379c2d1d19f2835bbe58d3f9fc148
Magician909 <78997718+EricFor@users.noreply.github.com>
2022-04-27 15:47:04 -0400

    Add files via upload

---
+++ GamePad.java
@@ -0,0 +1,24 @@
+public class GamePad {
+
+  //fields and methods for gamepad class 
+  
+boolean x;
+boolean y;
+boolean a;
+boolean b; 
+
+  //Obviously wrong logic, but Gamepad is out of the scope of this lesson 
+  
+  public boolean isAPressed(){
+    return false;
+  }
+  public boolean isBPressed(){
+    return false;
+  }
+  public boolean isXPressed(){
+    return false;
+  }
+  public boolean isYPressed(){
+    return false;
+  }
+}
---
+++ SubSystem.java
@@ -0,0 +1,7 @@
+public abstract class SubSystem{
+  public GamePad gamepad;   
+  public abstract void init();
+  public abstract void update();
+
+
+}
